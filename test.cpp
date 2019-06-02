d3b0781c0da36b9671ec053b3f5c19e581427a4a
ZiDong Fang <424044876@qq.com>
2019-06-02 19:59:40 +0800

    0.1.4
    想了想怎么实现。
    另外，决定用stdio写文件了

--- test.cpp
+++ test.cpp
@@ -3,6 +3,15 @@
 //
 
 #include <iostream>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <limits.h>
+#include <string.h>
+
 #include "file_system.h"
 #include "inode.h"
 #include "dir.h"
@@ -11,9 +20,13 @@ using namespace std;
 
 
 int main(){
-    I_NODE r;
-    cout<< sizeof(r);
-    cout<< sizeof(DIR);
+    DIR dir;
+//    dir.inode_pointer[0]=111;
+    FILE *f;
+    f=fopen("/tmp/test_data", "rb+");
+    fread(&dir, sizeof(dir), sizeof(dir), f);
+    printf("%d\n", dir.inode_pointer[0]);
+    fclose(f);
     return 0;
 }
 
